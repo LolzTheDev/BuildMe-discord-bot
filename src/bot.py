7211df9c7281798ef5b2762a13e07e519455e8fd
LolzTheGreat <141521874+LolzTheDev@users.noreply.github.com>
2023-10-08 10:09:00 -0500

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -12,5 +12,20 @@ async def ping(ctx):
     ping_time = (ending_time - starting_time) * 1000
     await msg.edit(content=f"The ping is: {ping_time:.2f}ms")
 
+@bot.command()
+async def credits(ctx):
+    msg = "CREDITS:\n"
+    
+    with open("assets/CREDITS.txt", "r") as file:
+        credits = file.readlines()
+
+    for line in credits:
+        if line.startswith("#"):
+            pass #comment
+        else:
+            msg = msg + line + "\n"
+
+    await ctx.send(msg)
+
 if __name__ == "__main__":
     bot.run("")
